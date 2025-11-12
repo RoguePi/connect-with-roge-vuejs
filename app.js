a664400779c0445f0d4470f4e0b177bdaabfff0d
RoguePi <rogelynpizon@gmail.com>
2025-11-13 06:52:06 +0800

    policies added

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ const App = {
                     <div class="footer-left">
                         <p>&copy; 2024 Rogelyn Pizon. Built with Vue.js</p>
                         <div class="legal-links">
-                            <a href="#" onclick="displayPreferenceModal();return false;" id="termly-consent-preferences">Cookie Preferences</a>
-                            <a href="#" data-termly-policy="privacy-policy">Privacy Policy</a>
+                            <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=9442a5ea-6484-4199-adec-d01e9ff402a5" data-termly-policy="cookie-policy" >Cookie Preferences</a>
+                            <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=cd733249-ef89-4304-befa-4ce0c6b26f42" data-termly-policy="privacy-policy">Privacy Policy</a>
                             <a href="#" data-termly-policy="terms-of-service">Terms of Service</a>
+                            <a href="#" onclick="displayPreferenceModal();return false;" id="termly-consent-preferences" class="termly-display-preferences">Consent Preferences</a>
                         </div>
                     </div>
                     <div class="footer-links">
